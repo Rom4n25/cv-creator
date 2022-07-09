03e41102930f9425683fef6649bd8db4a65c76e2
roman <mromanek1802@gmail.com>
2022-07-09 14:24:29 +0200

    Updates

---
+++ precache-manifest.1fef8ba4eca559fe9ac1793576e271e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6002845904cd2f4b981",
+    "url": "/cv-creator/static/css/main.8fb3b6c9.chunk.css"
+  },
+  {
+    "revision": "e6002845904cd2f4b981",
+    "url": "/cv-creator/static/js/main.e6002845.chunk.js"
+  },
+  {
+    "revision": "dbbc9db2c62956b49d2f",
+    "url": "/cv-creator/static/js/1.dbbc9db2.chunk.js"
+  },
+  {
+    "revision": "4c2426c4cb9e076857f9",
+    "url": "/cv-creator/static/css/2.5cd5463d.chunk.css"
+  },
+  {
+    "revision": "4c2426c4cb9e076857f9",
+    "url": "/cv-creator/static/js/2.4c2426c4.chunk.js"
+  },
+  {
+    "revision": "274291ab7817459b02c5",
+    "url": "/cv-creator/static/js/3.274291ab.chunk.js"
+  },
+  {
+    "revision": "a34ecb29399f8ea1f581",
+    "url": "/cv-creator/static/js/runtime~main.a34ecb29.js"
+  },
+  {
+    "revision": "7ef213ce4f9386a5aa577414fbad5050",
+    "url": "/cv-creator/static/media/person.7ef213ce.jpg"
+  },
+  {
+    "revision": "586e1772ee97c2063f786a3844b0ce52",
+    "url": "/cv-creator/index.html"
+  }
+];
